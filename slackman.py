59f766a1d20a079a25b1d09fc927d8c2300f9118
Emil Smith <smithomaster@gmail.com>
2018-03-29 20:44:24 +1030

    deletion of pygamemenu

--- slackman.py
+++ slackman.py
@@ -1,24 +1,19 @@
-import pygame, sys
-import pygameMenu
-from pygameMenu.locals import *
+import pygame
 
 window_width = 600
 window_height = 600
-font = "8bit"
-title = "SlackMan"
 pygame.init()
 screen = pygame.display.set_mode((window_width, window_height))
-menu = pygameMenu.Menu(screen, window_width=window_width, window_height=window_height, font=pygameMenu.fonts.FONT_NEVIS, title='Main Menu')
 finished = False
 start = False
+emil = 0
 
 while not finished:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             finished = True
-    pygame.display.flip()
+        pygame.display.flip()
 
 pygame.quit()
