1dee158775ec6aec2faeca61e5389ef55741c34a
Emil Smith <smithomaster@gmail.com>
2018-04-04 09:43:45 +0930

    work of 4/04/18
    woooooo double lesson

--- slackman.py
+++ slackman.py
@@ -1,19 +1,91 @@
+# Game by Jacob Ditter and Emil Smith
+# Menu Elements in part by Harrison@pythonprogramming.net
+
 import pygame
-
-window_width = 600
-window_height = 600
+import time
+import random
+ 
 pygame.init()
-screen = pygame.display.set_mode((window_width, window_height))
-finished = False
-start = False
-emil = 0
+ 
+display_width = 800
+display_height = 600
+ 
+black = (0,0,0)
+white = (255,255,255)
+red = (200,0,0)
+green = (0, 200, 0)
+bright_green = (0, 255, 0)
+bright_red = (255, 0, 0)
 
-while not finished:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            finished = True
-        pygame.display.flip()
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('SlackMan')
+clock = pygame.time.Clock()
+ 
+carImg = pygame.image.load('dotter.png')
+ 
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+ 
+def message_display(text, color):
+    largeText = pygame.font.Font('freesansbold.ttf',115, color)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+ 
+    pygame.display.update()
+ 
+    time.sleep(2)
+ 
+    game_loop()
 
+def game_loop():
+    gameDisplay.fill(black)
+    message_display("Welcome to SlackMan", white)
+    pygame.display.update()
+
+def button(msg, x, y, w, h, ic, ac, action = None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            if action == "play:":
+                return
+                game_loop()
+            elif action == "quit":
+                pygame.quit()
+                quit()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
+
+    smallText = pygame.font.Font("freesansbold.ttf", 32)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2), y + (h/2)))
+    gameDisplay.blit(textSurf, textRect)
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('8bit.ttf',90)
+        TextSurf, TextRect = text_objects("SlackMan", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("Start Game", 150, 400, 200, 100, green, bright_green, "play")
+        button("Exit", 450, 400, 200, 100, red, bright_red, "quit")
+
+        pygame.display.update()
+
+game_intro()
+game_loop()
 pygame.quit()
+quit()
